4cb8344829bc8c8ec7bc158316fd91a716b909ff
NikitaS <sergio791309@gmail.com>
2022-07-08 22:10:02 +0300

    Третья задача

---
+++ examples/Program.cs
@@ -0,0 +1,24 @@
+﻿//int squareNumber = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Введите второе число: ");
+//int number = Convert.ToInt32(Console.ReadLine());
+//if (squareNumber == number * number)
+//{
+//   Console.WriteLine("Число " +squareNumber + " является квадратом " + number );
+//}
+//else
+//{
+//    Console.WriteLine("Число " +squareNumber  + " НЕявляется квадратом " + number );
+//}
+Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negativeN = N * (-1);
+
+while (negativeN <= N)
+{
+     Console.WriteLine(negativeN);
+    negativeN++;
+}
+
+
+
+
